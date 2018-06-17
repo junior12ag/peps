05f76c05b985b7708a599d5ae359c9d60457596e
Junior Aguiar <junior12ag@gmail.com>
2018-06-17 19:10:35 -0400

    Primer commit

--- peps/settings.py
+++ peps/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
